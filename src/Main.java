5a6c25a0274d1c201ee4a6c0650c00ce4d88ef39
alisazoo <arisa-shinozuka@hotmail.com>
2021-06-14 10:53:12 +0900

    Add user input (for floor size and a piece of furniture)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class Main {
-    public static void main(String[] args) {
+    public static <numOfFurniture> void main(String[] args) {
+
+        int myFloorLength, myFloorWidth, myFurnitureLength, myFurnitureWidth;
+        String myFurnitureName;
+        Floor myFloor;
+        ArrayList<Furniture> myFurniture = new ArrayList<>();
 
         // Ask the size of floor
-
-        // Ask the size of a furniture
+        Scanner prompt = new Scanner(System.in);
+        System.out.println("Enter the length of your floor in millimeters (e.g. 3650)");
+        myFloorLength = parseInt(prompt.nextLine());
+        System.out.println("Enter the width of your floor in millimeters (e.g. 2170)");
+        myFloorWidth = parseInt(prompt.nextLine());
 
         // Set the size of floor
-        Floor floor = new Floor(100,200);
-        // Set the size of furniture
-        Furniture furniture1 = new Furniture(10, 45, "shelf");
+        myFloor = new Floor(myFloorLength,myFloorWidth);
+
+        // Ask the size of a furniture
+        System.out.println("Do you have any furniture?: Y/N");
+        String ans = prompt.nextLine();
+        if(ans.toLowerCase().equals("n")){
+            System.out.println("Ok, that's done");
+        } else{
+            System.out.println("Please enter the name of the furniture:");
+            myFurnitureName = prompt.nextLine();
+            System.out.println("Please enter the length of " + myFurnitureName + ":");
+            myFurnitureLength = parseInt(prompt.nextLine());
+            System.out.println("Please enter the width of " + myFurnitureName + ":");
+            myFurnitureWidth = parseInt(prompt.nextLine());
+            // Set the size of furniture
+            myFurniture.add(new Furniture(myFurnitureLength, myFurnitureWidth, myFurnitureName));
+        }
 
         // Display the values of floor and furniture
         System.out.println("The size of the floor: ");
-        System.out.println(floor.getLength() + " x " + floor.getWidth()
+        System.out.println(myFloor.getLength() + " x " + myFloor.getWidth()
                 + " (length x width)");
-        System.out.println("The size of " + furniture1.getName());
-        System.out.println(furniture1.getLength() + " x " + furniture1.getWidth()
-                + " (length x width)");
-        System.out.println(furniture1.isAvailable);
+        if (myFurniture.size() != 0) {
+            for (Furniture item : myFurniture) {
+                System.out.println("The size of " + item.getName());
+                System.out.println(item.getLength() + " x " + item.getWidth()
+                        + " (length x width) (" + item.isAvailable + ")");
 
+            }
+        }
     }
 }

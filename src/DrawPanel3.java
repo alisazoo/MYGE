8ac5bfe15c192a2d9f73c7936c76124c860006b6
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-06-21 12:19:40 +0900

    Minor change: Change the variables that are used across multiple classes to private class instance variables from inner variables.

--- src/DrawPanel3.java
+++ src/DrawPanel3.java
@@ -5,8 +5,9 @@ import java.awt.event.ActionListener;
 
 public class DrawPanel3 extends JPanel {
 
-    DataPanel dataPanel;
-    FloorPanel floorPanel;
+    private DataPanel dataPanel;
+    private FloorPanel floorPanel;
+    private JTextField floorWidthTxt, floorLengthTxt;
 
     DrawPanel3(){
         setBackground(Color.DARK_GRAY);
@@ -17,14 +18,11 @@ public class DrawPanel3 extends JPanel {
         setLayout(new BorderLayout());
         add(dataPanel, BorderLayout.SOUTH);
         add(floorPanel,BorderLayout.CENTER);
-//        repaint();
     } // end: Constructor DrawPanel3
 
     private class DataPanel extends JPanel implements ActionListener {
 
-        Floor floor;
         JLabel floorName, xLabel;
-        JTextField floorWidth, floorLength;
         JButton setBtn;
 
         DataPanel(){
@@ -32,68 +30,67 @@ public class DrawPanel3 extends JPanel {
             setForeground(Color.BLACK);
 
             xLabel = new JLabel(" x ");
-
             floorName = new JLabel("Floor");
-
-            floorWidth = new JTextField(" (width: mm)", 10);
-            floorLength = new JTextField(" (length: mm)", 10);
-
+            floorWidthTxt = new JTextField("0", 10);
+            floorLengthTxt = new JTextField("0", 10);
             setBtn = new JButton("Set");
-//            setBtn.addActionListener(floorPanel);
+            setBtn.addActionListener(floorPanel);
             setBtn.addActionListener(this);
-            // Layout
-//        setLayout(new GridLayout(0,3));
-            // show only user input and xLabel, mmLabel, and setBtn.
-            //TODO change the layout with more components.
 
-            add(floorWidth);
+//            // Layout
+//            setLayout(new GridLayout(0,3));
+//            // show only user input and xLabel, mmLabel, and setBtn.
+//            //TODO change the layout with more components.
+
+            add(floorWidthTxt);
             add(xLabel);
-            add(floorLength);
+            add(floorLengthTxt);
             add(setBtn);
-
-            repaint();
 
         }
 
         public void actionPerformed(ActionEvent evt){
             String cmd = evt.getActionCommand();
             if( cmd.equals("Set"))
-                setBtn.setText("I am listenig!");
+                setBtn.setText("I am listening!");
             repaint();
         }
-    }// end: nested-inner class DataPanel
+    } // end: nested-inner class DataPanel
 
     public class FloorPanel extends JPanel implements ActionListener {
 
-        Floor floor;
-        JLabel floorWidth, floorLength, notificationLabel;
+        JLabel notificationLabel;
 
         public FloorPanel(){
             setBackground(Color.WHITE);
             setPreferredSize( new Dimension(400,300) );
 
-            notificationLabel = new JLabel("Input the size of floor, then click set button.");
+            notificationLabel = new JLabel(
+                    "Input the size of floor, then click set button.",
+                    JLabel.CENTER);
 
             setLayout( new GridLayout(3,2) );
             add(notificationLabel);
-
-        }
+        } // end: constructor
 
         @Override
         public void actionPerformed(ActionEvent evt) {
             String cmd = evt.getActionCommand();
-            if( cmd.equals("Set")) {
+            if( cmd.equals("Set") || cmd.equals("I am listening!")) {
                 //TODO set the values for floor
-                JLabel floorData = new JLabel();
-                String floorWidthText = floorWidth.getText();
-                String floorLengthText = floorLength.getText();
-                String floorText = "The size of your floor is " + floorWidthText +
+//                JLabel floorData = new JLabel();
+                String floorWidthText = floorWidthTxt.getText();
+                String floorLengthText = floorLengthTxt.getText();
+                String floorNotification = "The size of your floor is " + floorWidthText +
                         " mm (width) x " + floorLengthText + " mm (length).";
-                floorData.setText(floorText);
-                add(floorData);
+                System.out.println(floorNotification);
+
+                notificationLabel.setText(floorNotification);
 
                 repaint();
             }
-        }
+        } // end: actionPerformed() method
+
     } // end: nested class FloorPanel
+
 }

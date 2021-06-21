1d7883e6788ac34d98bbae17f6e13e8b2da74281
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-06-21 14:44:41 +0900

    Added elements in floorPanel: - show the length and width of floor on the floorPanel. - display the floor image (the size is not changable and is 675 * 400 pixel).

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -8,7 +8,7 @@ public class DrawPanel extends JPanel {
     private DataPanel dataPanel;
     private FloorPanel floorPanel;
     private JTextField floorWidthTxt, floorLengthTxt;
-    private JLabel notificationLabel;
+    private JLabel notificationLabel, floorWidth, floorLength;
 
     DrawPanel(){
         setBackground(Color.DARK_GRAY);
@@ -42,9 +42,9 @@ public class DrawPanel extends JPanel {
 //            //TODO change the layout with more components.
 
             add(floorWidthTxt);
-            add(new JLabel(" x "));
+            add(new JLabel("(width) x "));
             add(floorLengthTxt);
-            add(new JLabel(" mm "));
+            add(new JLabel("(length) mm "));
             add(setBtn);
 
         }
@@ -52,12 +52,13 @@ public class DrawPanel extends JPanel {
         public void actionPerformed(ActionEvent evt){
             String cmd = evt.getActionCommand();
             if( cmd.equals("Set")) {
-                String floorWidthText = floorWidthTxt.getText();
-                String floorLengthText = floorLengthTxt.getText();
-                String floorNotification = "The size of your floor is " + floorWidthText +
-                        " mm (width) x " + floorLengthText + " mm (length).";
-                System.out.println(floorNotification);
+                String floorWidthText = floorWidthTxt.getText() + "mm (width)";
+                String floorLengthText = floorLengthTxt.getText() + " mm (length)";
+                String floorNotification = "The size of your floor: " + floorWidthText +
+                        " x " + floorLengthText + ".";
                 notificationLabel.setText(floorNotification);
+                floorWidth.setText(floorWidthText);
+                floorLength.setText(floorLengthText);
             }
             repaint();
         }
@@ -69,13 +70,28 @@ public class DrawPanel extends JPanel {
             setBackground(Color.WHITE);
             setPreferredSize( new Dimension(400,300) );
 
+            floorWidth = new JLabel("0 mm (width)", JLabel.CENTER);
+            floorLength = new JLabel("0 mm (length)", JLabel.CENTER);
+            floorLength.setUI(new VerticalLabelUI(true));
             notificationLabel = new JLabel(
                     "Input the size of floor, then click set button.",
                     JLabel.CENTER);
 
-            setLayout( new GridLayout(3,2) );
-            add(notificationLabel);
+            setLayout( new BorderLayout(10,10) );
+            add(floorWidth, BorderLayout.NORTH);
+            add(notificationLabel, BorderLayout.SOUTH);
+            add(floorLength, BorderLayout.EAST);
         } // end: constructor
+
+        /**
+         * This method draws the floor image in FloorPanel.
+         * @param g Graphic context
+         */
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            setForeground(Color.GRAY);
+            g.fillRect(50,50,675,400);
+        }
 
     } // end: nested class FloorPanel
 

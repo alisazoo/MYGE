dad8f9c87a660c2b7c0f115aed76e030f30531a9
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-06-30 22:15:10 +0900

    ex 6.3: Rolling dice ver.2, referring to the example answer. (there is still getGraphics() method, which is not recommended to use in a elegant code!)

--- src/ex6_3/RollDice.java
+++ src/ex6_3/RollDice.java
@@ -11,28 +11,47 @@ package ex6_3;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class RollDice extends JPanel implements MouseListener {
+public class RollDice extends JPanel {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Rolling dice sounds fun");
+
         RollDice content = new RollDice();
+        content.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
+//        content.setBackground(Color.PINK);
+
         window.setContentPane(content);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setLocation(120,60);
-        content.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
-        content.setBackground(Color.PINK);
-        window.setSize(new Dimension(145,170));
-//        window.pack();
+        window.setLocation(120,70);
+//        window.setSize(new Dimension(145,170));
+        window.pack(); // need to set the preferred size of the size (see below)
         window.setVisible(true);
     }
 
+    /**
+     * The constructor adds a mouse listener to the panel.
+     * The listener will roll the dice when the user clicks the panel.
+     * Also, the background colour and preferred size of the panel are set.
+     */
     public RollDice(){
-        addMouseListener(this);
+        setPreferredSize(new Dimension(125,125));
+        setBackground(new Color(200,200,255)); // Light Blue
+//        addMouseListener(this);
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                Graphics g = getGraphics();
+                int randomNum1 = (int)(Math.random()*6) + 1;
+                int randomNum2 = (int)(Math.random()*6) + 1;
+                int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
+                drawDie(g, randomNum1, x1,y1);
+                drawDie(g, randomNum2, x2,y2);
+                g.dispose();
+            }
+        });
     }
 
     /**
@@ -87,27 +106,30 @@ public class RollDice extends JPanel implements MouseListener {
 
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         int randomNum1 = (int)(Math.random()*6) + 1;
         int randomNum2 = (int)(Math.random()*6) + 1;
         int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
-        drawDie(g, randomNum1, x1,y1);
-        drawDie(g, randomNum2, x2,y2);
+        drawDie(g2, randomNum1, x1,y1);
+        drawDie(g2, randomNum2, x2,y2);
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        Graphics g = getGraphics();
-        int randomNum1 = (int)(Math.random()*6) + 1;
-        int randomNum2 = (int)(Math.random()*6) + 1;
-        int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
-        drawDie(g, randomNum1, x1,y1);
-        drawDie(g, randomNum2, x2,y2);
-        g.dispose();
-    }
+//    @Override
+//    public void mousePressed(MouseEvent e) {
+//        Graphics g = getGraphics();
+//        int randomNum1 = (int)(Math.random()*6) + 1;
+//        int randomNum2 = (int)(Math.random()*6) + 1;
+//        int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
+//        drawDie(g, randomNum1, x1,y1);
+//        drawDie(g, randomNum2, x2,y2);
+//        g.dispose();
+//    }
 
-    public void mouseClicked(MouseEvent e) { }
-    public void mouseReleased(MouseEvent e) { }
-    public void mouseEntered(MouseEvent e) { }
-    public void mouseExited(MouseEvent e) { }
+//    public void mouseClicked(MouseEvent e) { }
+//    public void mouseReleased(MouseEvent e) { }
+//    public void mouseEntered(MouseEvent e) { }
+//    public void mouseExited(MouseEvent e) { }
 
 }

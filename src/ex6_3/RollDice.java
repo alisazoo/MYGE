3064bc7e3c21db4cd06b085316e0264d2d50a169
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-07-01 10:31:35 +0900

    ex 6.3: Clean Code and apply to the way in the example answer. (Leave diff to show the difference of the logic).

--- src/ex6_3/RollDice.java
+++ src/ex6_3/RollDice.java
@@ -1,36 +1,26 @@
-/**
- * 30/6/2021
- * I tried but could not nail it!
- * Issue1: graphic context = null, so compile time error occurs before nothing is drawn.
- *  >>> solved. Graphics context is obtained automatically by the system.
- * Issye2: drawDie() method is repeatedly invoked (almost forever).
- *  >>> solved. Delete unnecessary repaint() method.
- */
-
 package ex6_3;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class RollDice extends JPanel {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Rolling dice sounds fun");
-
         RollDice content = new RollDice();
-        content.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
-//        content.setBackground(Color.PINK);
-
         window.setContentPane(content);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocation(120,70);
-//        window.setSize(new Dimension(145,170));
         window.pack(); // need to set the preferred size of the size (see below)
         window.setVisible(true);
     }
+
+    //----------------------------------------------------------------
+
+    private int die1 = 3;
+    private int die2 = 4;
 
     /**
      * The constructor adds a mouse listener to the panel.
@@ -40,17 +30,19 @@ public class RollDice extends JPanel {
     public RollDice(){
         setPreferredSize(new Dimension(125,125));
         setBackground(new Color(200,200,255)); // Light Blue
-//        addMouseListener(this);
+        setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                Graphics g = getGraphics();
-                int randomNum1 = (int)(Math.random()*6) + 1;
-                int randomNum2 = (int)(Math.random()*6) + 1;
-                int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
-                drawDie(g, randomNum1, x1,y1);
-                drawDie(g, randomNum2, x2,y2);
-                g.dispose();
+                roll();
             }
+//                Graphics g = getGraphics();
+//                int randomNum1 = (int)(Math.random()*6) + 1;
+//                int randomNum2 = (int)(Math.random()*6) + 1;
+//                int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
+//                drawDie(g, randomNum1, x1,y1);
+//                drawDie(g, randomNum2, x2,y2);
+//                g.dispose();
+//            }
         });
     }
 
@@ -104,32 +96,25 @@ public class RollDice extends JPanel {
         }
     }
 
+    /**
+     * Roll the dice by randomising that their values.
+     * Tell the system to repaint the panel, to show the new value.
+     */
+    public void roll(){
+        die1 = (int)(Math.random()*6) + 1;
+        die2 = (int)(Math.random()*6) + 1;
+        repaint();
+    }
+
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        int randomNum1 = (int)(Math.random()*6) + 1;
-        int randomNum2 = (int)(Math.random()*6) + 1;
-        int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
-        drawDie(g2, randomNum1, x1,y1);
-        drawDie(g2, randomNum2, x2,y2);
-    }
-
-//    @Override
-//    public void mousePressed(MouseEvent e) {
-//        Graphics g = getGraphics();
 //        int randomNum1 = (int)(Math.random()*6) + 1;
 //        int randomNum2 = (int)(Math.random()*6) + 1;
-//        int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
-//        drawDie(g, randomNum1, x1,y1);
-//        drawDie(g, randomNum2, x2,y2);
-//        g.dispose();
-//    }
-
-//    public void mouseClicked(MouseEvent e) { }
-//    public void mouseReleased(MouseEvent e) { }
-//    public void mouseEntered(MouseEvent e) { }
-//    public void mouseExited(MouseEvent e) { }
-
+        int x1 = 10, y1 = 10, x2 = 65, y2 = 65;
+        drawDie(g2, die1, x1,y1);
+        drawDie(g2, die2, x2,y2);
+    }
 }

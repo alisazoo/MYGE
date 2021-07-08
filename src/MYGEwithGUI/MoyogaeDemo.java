3de5ec7721588f4cd39f4c1696f23f55a001fb41
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-07-08 10:29:24 +0900

    Debugging: fix the issue around adding new piece of furniture that has the same name as the existing one.

--- src/MYGEwithGUI/MoyogaeDemo.java
+++ src/MYGEwithGUI/MoyogaeDemo.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MoyogaeDemo extends JPanel {
 
@@ -16,7 +17,7 @@ public class MoyogaeDemo extends JPanel {
     JList<String> furnitureList;
 
     static boolean isDuplicateFurniture = false;
-    static String[] result;
+    static String[] result = new String[3];
 
     /**
      * This main routine allow to use this program as an application.
@@ -32,8 +33,7 @@ public class MoyogaeDemo extends JPanel {
      */
     public void buildGUI(){
 
-        // TODO: delete the following test data after implement input dialog.
-        // for debugging
+        // TODO: delete the following test data
         Furniture.addFurniture("desk", 120,60);
         Furniture.addFurniture("Shelf", 40,30);
         Furniture.addFurniture("Chair", 60,60);
@@ -135,6 +135,7 @@ public class MoyogaeDemo extends JPanel {
      */
     public static void inputFloorData(){
 
+// TODO: uncomment out the following code to use dialog input
         // Input Dialog
 //        JPanel panel = new JPanel(new BorderLayout(5,5));
 //
@@ -159,7 +160,7 @@ public class MoyogaeDemo extends JPanel {
 //        Floor.setWidth( Integer.parseInt(floorWidthInput.getText()) );
 //        Floor.setLength( Integer.parseInt(floorLengthInput.getText()) );
 
-        // for debugging: skip the first input dialog
+        // TODO: delete the following test data
         Floor.setWidth(3000);
         Floor.setLength(2100);
 
@@ -170,69 +171,76 @@ public class MoyogaeDemo extends JPanel {
         public void actionPerformed(ActionEvent evt){
             if(!listModel.isEmpty()) {
                 int index = furnitureList.getSelectedIndex();
+                String v = furnitureList.getSelectedValue();
+
                 listModel.removeElementAt(index);
+
+                //TODO: the item should be deleted from Furniture.furnitureArrayList so
+                //      create extractString method and add here.
+
             } else {
+                // TODO: replace with dialog window.
                 System.out.println("no more item here. please add the new one!");
             }
         }
     }
 
     /**
-     * This inputFurnitureData method set the new item in the ArrayList<Furniture>.
+     * This static inputFurnitureData method aims to set the new item in the ArrayList<Furniture>.
+     * The data of the new item is sent from input dialog.
      */
     public static void inputFurnitureData(
             ArrayList<Furniture> arrayList, DefaultListModel<String> listModel){
 
-        result = inputFurnitureDialog();
+        do{
+            result = inputFurnitureDialog();
+        } while (isDuplicateFurniture);
 
-        // Set the input data into the ArrayList
+        // Set the input data into the local variables
         Furniture item = new Furniture();
         String name = result[0];
         int width, length;
         width = Integer.parseInt( result[1] );
         length = Integer.parseInt( result[2] );
 
-        for (Furniture newItem: Furniture.furnitureArrayList){
-            if(newItem.getName().equals(name)){
-                System.out.println(newItem.getName());
-                isDuplicateFurniture = true;
-            }
-        }
-        if(isDuplicateFurniture) { // If the name of the item is the same as the existing items
-            //TODO: it is not good never to use result array. Need to improve!
-            result = new String[]{};
-            result = inputFurnitureDialog();
+        // new data is assigned to the ArrayList
+        item.setName(name);
+        item.setWidth(width);
+        item.setLength(length);
+        arrayList.add(item);
 
-        } else {
-            item.setName(name);
-            item.setWidth(width);
-            item.setLength(length);
-            arrayList.add(item);
+        // Set the input data into the DefaultListModel
+        String newItemTxt = name + ": " + width + " mm x " + length + " mm";
+        listModel.addElement(newItemTxt);
 
-            // Set the input data into the DefaultListModel
-            String newItemTxt = name + ": " + width + " mm x " + length + " mm";
-            listModel.addElement(newItemTxt);
-        }
     }   // end inputFurnitureData() method
 
+    /**
+     * This static method inputFurnitureDialog create the input dialog
+     * for user to input the data of new furniture item. If the input name of the new item
+     * is same as the existing one, this program requires to input again.
+     * @return array with String value of name, width, and length of the new item.
+     */
     public static String[] inputFurnitureDialog(){
 
-        // Input Dialog
+        // if any elements of invalid data in result, delete them
+        if( isDuplicateFurniture ){
+            //TODO: find more sophisticated way would be better.
+            result = new String[3];
+        }
+
+        // set up the main panel for Input Dialog
         JPanel panel = new JPanel(new BorderLayout(5,5));
 
+        // set up labelPanel
         JPanel label = new JPanel(new GridLayout(0,1,2,2));
         label.add( new JLabel("Name", SwingConstants.RIGHT) );
         label.add( new JLabel("Width (mm)", SwingConstants.RIGHT) );
         label.add( new JLabel("Length (mm)", SwingConstants.RIGHT) );
         panel.add( label, BorderLayout.WEST );
 
+        // set up controlPanel
         JPanel control = new JPanel(new GridLayout(0,1,2,2));
-
-        if(isDuplicateFurniture)
-            panel.add( new JLabel(
-                    "You cannot add duplicated item. Please make the new one with different name."),
-                    BorderLayout.NORTH);
-
         JTextField furnitureName = new JTextField();
         control.add( furnitureName );
         JTextField furnitureWidthInput = new JTextField();
@@ -240,18 +248,58 @@ public class MoyogaeDemo extends JPanel {
         JTextField furnitureLengthInput = new JTextField();
         control.add(furnitureLengthInput);
 
+        // add components onto the main panel
+        if(isDuplicateFurniture) {
+            panel.add( new JLabel("You cannot add duplicated item. " +
+                    "Please make the new one with different name."), BorderLayout.NORTH );
+        } else {
+            panel.add( new JLabel("Enter the data of the new item."), BorderLayout.NORTH);
+        }
         panel.add(control, BorderLayout.CENTER);
 
         JOptionPane.showMessageDialog(null, panel,
                 "Add new furniture", JOptionPane.QUESTION_MESSAGE);
 
+        // keep the input data into the String array, infoArray.
+        String name = furnitureName.getText();
+        String width = furnitureWidthInput.getText();
+        String length = furnitureLengthInput.getText();
+
         String[] infoArray = new String[3];
-        infoArray[0] = furnitureName.getText();
-            // The value is not trimmed considering the possiblity to treat not only single word name.
-        infoArray[1] = furnitureWidthInput.getText();
-        infoArray[2] = furnitureLengthInput.getText();
+        infoArray[0] = name;
+            // The value is not trimmed considering the possibility to treat not only single word name.
+            //TODO: trim the word?
+        infoArray[1] = width;
+        infoArray[2] = length;
+
+        // If the name of the new input item is the same as one of the existing item,
+        // isDuplicateFurniture set as true.
+        for (Furniture newItem: Furniture.furnitureArrayList){
+            if(newItem.getName().equals(name)){
+                 isDuplicateFurniture = true;
+                 break;
+            } else {
+                isDuplicateFurniture = false;
+            }
+        }
 
         return infoArray;
+    }
+
+    private String extractString(String targetStr){
+
+        String extractedName = "";
+
+        //TODO: create the method based on https://www.baeldung.com/java-substring
+        try (Scanner scanner = new Scanner(targetStr)){
+            scanner.useDelimiter(":");
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return extractedName;
     }
 
 

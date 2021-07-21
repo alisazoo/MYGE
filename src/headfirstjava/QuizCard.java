ff9d0e264da30d74bd221e05c53f9986fd968447
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-07-21 11:19:34 +0900

    ch14: Serialization and file I/O.

---
+++ src/headfirstjava/QuizCard.java
@@ -0,0 +1,28 @@
+package headfirstjava;
+
+public class QuizCard {
+
+    private String answer;
+    private String question;
+
+    public QuizCard( String question, String answer){
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}

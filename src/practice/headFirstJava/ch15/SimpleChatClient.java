8dc0ff8f1e69665f1c89706ccfd627329d27695c
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-07-26 12:49:48 +0900

    ch15: Networking and thread - simple chat application (final ver.) but the input cannot be reflected on GUI...

--- src/practice/headFirstJava/ch15/SimpleChatClient.java
+++ src/practice/headFirstJava/ch15/SimpleChatClient.java
@@ -12,7 +12,8 @@ import java.net.Socket;
 
 public class SimpleChatClient {
 
-    JTextArea  incoming;
+    JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+    JTextArea incoming;
     JTextField outgoing;
     BufferedReader reader;
     PrintWriter writer;
@@ -23,43 +24,49 @@ public class SimpleChatClient {
         client.go();
     }
 
-    public void go(){
 
-        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+    public void go(){
         JPanel mainPanel = new JPanel();
+
         incoming = new JTextArea(15,50);
         incoming.setLineWrap(true);
         incoming.setWrapStyleWord(true);
         incoming.setEditable(false);
+
         JScrollPane qScroller = new JScrollPane(incoming);
-        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener( new SendButtonListener() );
+
         mainPanel.add(qScroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
+
         setUpNetworking();
 
-        Thread readerThread = new Thread(new IncomingReader() );
+        Thread readerThread = new Thread( new IncomingReader() );
         readerThread.start();
 
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-        frame.setSize(800, 500);
+        frame.setSize(400,500);
         frame.setVisible(true);
     }
 
     private void setUpNetworking(){
         try{
-//            sock = new Socket("127.0.0.1", 5000);
-            sock = new Socket("126.209.37.218", 5000);
+            sock = new Socket("localhost", 5000);
+
             InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
             reader = new BufferedReader(streamReader);
+
             writer = new PrintWriter(sock.getOutputStream());
-            System.out.println("networking established");
-        } catch (IOException ie){
-            ie.printStackTrace();
+
+            System.out.println("Networking established");
+        } catch (IOException ioException){
+            ioException.printStackTrace();
         }
     }
 
@@ -69,11 +76,12 @@ public class SimpleChatClient {
             try{
                 writer.println(outgoing.getText());
                 writer.flush();
-            } catch (Exception ex){
-                ex.printStackTrace();
+            } catch(Exception exception){
+                exception.printStackTrace();
             }
             outgoing.setText("");
             outgoing.requestFocus();
+            frame.repaint();
         }
     }
 
@@ -82,36 +90,14 @@ public class SimpleChatClient {
         public void run() {
             String message;
             try{
-
-                while( (message = reader.readLine()) != null ){
+                while( (message = reader.readLine()) != null){
                     System.out.println("read " + message);
                     incoming.append(message + "\n");
                 }
-            } catch(Exception ex){
-                ex.printStackTrace();
+            } catch (Exception e){
+                e.printStackTrace();
             }
         }
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
